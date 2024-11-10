6fe17ddd1d34ae71171919460e638a43b209733e
Steven Rugg <steven.rugg@outlook.com>
2024-11-10 08:29:29 -0800

    fix errors

---
+++ src/app/api/guestbook/route.ts
@@ -0,0 +1,38 @@
+// src/app/api/guestbook/route.ts
+import { NextResponse } from 'next/server';
+import { auth } from '@/app/auth' 
+import { guestbookSchema } from '@/lib/validationSchemas';
+import prisma from '@/lib/prisma';
+
+// POST /api/guestbook - Adds a new guestbook message
+export async function POST(req: Request) {
+  const session = await auth()
+  
+  if (!session) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  try {
+    const data = await req.json();
+    const validatedData = guestbookSchema.parse(data);
+
+    const newMessage = await prisma.guestbook.create({
+      data: validatedData,
+    });
+
+    return NextResponse.json(newMessage);
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.errors || 'Invalid input' },
+      { status: 400 }
+    );
+  }
+}
+
+// GET /api/guestbook - Retrieves guestbook messages
+export async function GET() {
+  const messages = await prisma.guestbook.findMany({
+    orderBy: { createdAt: 'desc' },
+  });
+  return NextResponse.json(messages);
+}
