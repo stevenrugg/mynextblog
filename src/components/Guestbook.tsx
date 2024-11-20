c9c460bb4d1219bdf97256a204ad896d7c984e76
Steven Rugg <Steven.rugg@outlook.com>
2024-11-19 22:09:57 -0800

    Update Guestbook.tsx
    fixed api

--- src/components/Guestbook.tsx
+++ src/components/Guestbook.tsx
@@ -10,7 +10,7 @@ const Guestbook: React.FC = () => {
 
   useEffect(() => {
     const fetchMessages = async () => {
-      const res = await fetch('src/app/api/guestbook/');
+      const res = await fetch('/api/guestbook');
       const data = await res.json();
       setMessages(data);
     };
@@ -28,14 +28,14 @@ const Guestbook: React.FC = () => {
 
     try {
       schema.parse(formData);
-      await fetch('src/app/api/guestbook/', {
+      await fetch('/api/guestbook', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
 
       setFormData({ name: '', message: '' });
-      const res = await fetch('src/app/api/guestbook/');
+      const res = await fetch('/api/guestbook');
       setMessages(await res.json());
     } catch (err: any) {
       setError(err.errors?.[0]?.message || 'Invalid input');
