b036c35298bdac53264178a5d245acb67f5acce1
Steven Rugg <steven.rugg@outlook.com>
2024-11-21 19:09:27 -0800

    fix vaPI

--- src/components/Guestbook.tsx
+++ src/components/Guestbook.tsx
@@ -12,7 +12,7 @@ const Guestbook: React.FC = () => {
   
   useEffect(() => {
     const fetchMessages = async () => {
-      const res = await fetch(`../app/api/guestbook`);
+      const res = await fetch(`/api/guestbook`);
       const data = await res.json();
       setMessages(data);
     };
@@ -30,14 +30,14 @@ const Guestbook: React.FC = () => {
 
     try {
       schema.parse(formData);
-      await fetch(`../app/api/guestbook`, {
+      await fetch('/api/guestbook', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json'},
         body: JSON.stringify(formData),
       });
 
       setFormData({ name: '', message: '' });
-      const res = await fetch(`../app/api/guestbook`);
+      const res = await fetch('/api/guestbook');
       setMessages(await res.json());
     } catch (err: any) {
       setError(err.errors?.[0]?.message || 'Invalid input');
