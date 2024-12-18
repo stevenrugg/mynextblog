3221db6d31852c11fcbb72fdcebc5cd65306e868
Steven Rugg <steven.rugg@outlook.com>
2024-12-17 22:18:05 -0800

    added projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,6 +1,22 @@
 const projects = [
+  {
+    id: "1",
+    title: 'grabbr',
+    image: '/images/blog/grabbr.png',
+    description: 'A lightweight data fetching utility for JavaScript that includes automatic caching, time based revalidation and more.',
+    href: 'https://www.npmjs.com/package/grabbr',
+    longDescription: 'A small data fetching utility for JavaScript that includes automatic caching, time based revalidation, support for SSR and React Query. '
+  },
+  {
+    id: "2",
+    title: 'signal-server.io',
+    image: '/images/blog/signalserver.png',
+    description: 'A custom signaling server for implementing WebRTC applications',
+    href: 'https://www.npmjs.com/package/signal-server.io',
+    longDescription: 'A custom signaling server designed for WebRTC implementations. The server is built with node.js and WebSockets'
+  },
     {
-      id: "1",
+      id: "3",
       title: 'Animated Animals',
       image: '/images/blog/animatedanimals.png',
       description: 'HTML5, CSS3, and vanilla JavaScript. A cat, deer, wolf, and bear split up into SVG segments and animated.',
@@ -8,7 +24,7 @@ const projects = [
       longDescription: "Animated Animals was created with only HTML5, CSS3 and vanilla javascript. I took a svg of a bear, a wolf, a cat and a deer and, respectively, sliced each of them up into triangles then animated the transitions in between the animals."
     },
     {
-      id: "2",
+      id: "4",
       title: "iOS Calculator",
       image: "/images/blog/calc.png",
       description: "A working clone of the iOS calculator using HTML, CSS, and JavaScript",
@@ -17,7 +33,7 @@ const projects = [
     },
 
     {
-      id: "3",
+      id: "5",
       title: "seattlesupergeek.io",
       image: '/images/blog/seattlesupergeek.png',
       description: "This website and blog! I write articles about frontend technology, current events, quantum computing and anything else that interests me.",
@@ -25,7 +41,7 @@ const projects = [
       longDescription: "This blog and website. I coded all of this myself. I started with a barebones boilerplate next.js project and added all the pages, and functionality. The projects page is created using getStaticPaths and getStaticProps, all the pages are pre-rendered on the server so they are very fast and have good SEO"
     },
     {
-      id: "4",
+      id: "6",
       title: 'WebRTC Video Chat',
       image: '/images/blog/webrtcthumbnail.png',
       description: 'A WebRTC Video Chat app using Socket.io for signaling',
@@ -33,7 +49,7 @@ const projects = [
       longDescription: "This was the most difficult application I have ever coded. The WebRTC API references really don't do a lot to make you confident in using the technology. I chose to use signal.io as the signaling channel. It seemed the best way to signal in node.js. The only dev dependencies are, I think: typescript, tailwindcss and signal.io. I am now an expert on the javascript event loop and non-blocking I/O. The application is made with node.js and javascript. The html is rendered with a .ejs template"
     },
     {
-      id: "5",
+      id: "7",
       title: 'Sunsauna',
       image: '/images/blog/sunsauna.png',
       description: 'Website for a local small business selling infrared in-home saunas.',
@@ -41,7 +57,7 @@ const projects = [
       longDescription: "I built this website for Sunsauna in 2013. It was one of the very first websites that I built. I believe I used the boilerplate for bootstrap 3 and built the site up using just html and css and jQuery"
     },
     {
-      id: "6",
+      id: "8",
       title: 'Art of the Table',
       image: '/images/blog/art.png',
       description: 'Website for a fine dining restaurant with a constantly changing menu.',
