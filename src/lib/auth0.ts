c4e125dd2419df727c400c20038d90c402e581be
Steven Rugg <steven.rugg@outlook.com>
2025-01-22 22:10:01 -0800

    auth0

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,7 +1,7 @@
 import { initAuth0, Auth0Server } from '@auth0/nextjs-auth0';
 
 export const pageRouterAuth: Auth0Server = initAuth0({
-  auth0Logout: !(process.env.AUTH0_ISSUER_BASE_URL as string).startsWith('https://seattlesupergeek.vercel.app'),
+  auth0Logout: !(process.env.AUTH0_ISSUER_BASE_URL as string).startsWith(''),
   routes: {
     login: '/api/auth/login',
     callback: '/api/auth/callback',
